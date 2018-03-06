5ab9aed5b6a04adfbe81c2e530fdcbba6bb5f2f2
Hannah Zentner <hazentner@gmail.com>
2018-03-06 10:32:41 -0500

    starter code

---
+++ cipher_startercode.py
@@ -0,0 +1,9 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+def encode(message, shift):
+        ret = ""
+        '''This is the string we will return'''
+
+def decode(message, shift):
+	ret = ""
+        '''This is the string we will return'''	
